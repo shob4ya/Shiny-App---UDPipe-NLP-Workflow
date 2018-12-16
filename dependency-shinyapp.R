6ec4fc392af6858e8456064023b079dd6e3d3d7b
shob4ya <44426889+shob4ya@users.noreply.github.com>
2018-12-16 22:36:40 +0530

    Add files via upload

---
+++ dependency-shinyapp.R
@@ -0,0 +1,21 @@
+if (!require(shiny)){install.packages("shiny")}
+if (!require(udpipe)){install.packages("udpipe")}
+if (!require(textrank)){install.packages("textrank")}
+if (!require(lattice)){install.packages("lattice")}
+if (!require(igraph)){install.packages("igraph")}
+if (!require(ggraph)){install.packages("ggraph")}
+if (!require(wordcloud)){install.packages("wordcloud")}
+if (!require(stringr)){install.packages("stringr")}
+if (!require(readr)){install.packages("readr")}
+if (!require(rvest)){install.packages("rvest")}
+library(shiny)
+library(udpipe)
+library(textrank)
+library(lattice)
+library(igraph)
+library(ggraph)
+library(ggplot2)
+library(wordcloud)
+library(stringr)
+library(readr)
+library(rvest)
